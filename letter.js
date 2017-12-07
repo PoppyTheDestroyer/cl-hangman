829d30a4f7cb97e2d874bcb05ba2a67c86525982
Jason Saenz <jasonsaenz@Jasons-MacBook-Pro.local>
2017-12-06 23:52:28 -0600

    oh shit

--- letter.js
+++ letter.js
@@ -1,6 +1,17 @@
 function letterBreak(wordDestroy) {
     this.wordDestroy = wordDestroy;
-    this.lettersArray = function () 
+    this.lettersArray = function () {
+        for (var i = 0, j = 0; i < wordDestroy.length; i++) {
+            wordLetters[j] = wordDestroy.charAt(i);
+            j++
+            if (wordDestroy.charAt(i) != " ") {
+                wordLetters[j] = false;
+            }
+            else {
+                wordLetters[j] = true;
+            } j++
+        }
+    }
 }
 
 module.exports = letter;
