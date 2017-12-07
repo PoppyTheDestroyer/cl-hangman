7544d01df2c902836d629d12b22145c53a55475f
Jason Saenz <jasonsaenz@Jasons-MacBook-Pro.local>
2017-12-06 22:54:14 -0600

    new npm api

--- hangman.js
+++ hangman.js
@@ -1,13 +1,27 @@
 var inquirer = require("inquirer");
 var word = require("./word.js");
+var letter = require("./letter.js");
+var names = require("simpsons-names");
 var ls = require("ls");
 
+var randomName = names.random();
+console.log(randomName)
 inquirer
     .prompt([
+{
+    type: "confirm",
+    message: "Would you like to play a game? (y or n)",
+    name: "start",
+    default: true
+},
+{
 
+}
     ])
 
+//Once the game is started, a word must be selected.
 function game() {
 
 }
 
+var newWord = new wordToGuess(randomName);
